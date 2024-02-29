553bf1d8b3696b7d0084533c00ebd51bd78bdd85
liyijia <liyijiadou@gmail.com>
2024-02-29 20:45:33 +0300

    实现管理员查询分页功能

---
+++ proje/funding-online/atcrowdfunding01-admin-parent/atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/mapper/AdminMapper.java
@@ -0,0 +1,38 @@
+package com.atguigu.crowd.mapper;
+
+import com.atguigu.crowd.entity.Admin;
+import com.atguigu.crowd.entity.AdminExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface AdminMapper {
+    int countByExample(AdminExample example);
+
+    int deleteByExample(AdminExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Admin record);
+
+    int insertSelective(Admin record);
+
+    List<Admin> selectByExample(AdminExample example);
+
+    Admin selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Admin record, @Param("example") AdminExample example);
+
+    int updateByExample(@Param("record") Admin record, @Param("example") AdminExample example);
+
+    int updateByPrimaryKeySelective(Admin record);
+
+    int updateByPrimaryKey(Admin record);
+
+    /**
+     * 分页显示Admin列表
+     * @param keyword 搜索关键字
+     * @return Admin列表
+     */
+    List<Admin> selectAdminByKeyword(String keyword);
+
+}
---
+++ proje/funding-online/atcrowdfunding01-admin-parent/atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/mvc/handler/AdminHandler.java
@@ -0,0 +1,73 @@
+package com.atguigu.crowd.mvc.handler;
+
+import com.atguigu.crowd.constant.CrowdConstant;
+import com.atguigu.crowd.entity.Admin;
+import com.atguigu.crowd.service.api.AdminService;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created: 2024/2/29
+ * Author: Li Yijia
+ * Description:
+ */
+@Controller
+public class AdminHandler {
+
+    @Autowired
+    private AdminService adminService;
+
+    @RequestMapping("/admin/get/page.html")
+    public String getPageInfo(
+            // 使用@RequestParam注解的defaultValue属性，指定默认值，在请求中没有携带对应参数时使用默认值
+            // keyword默认值使用空字符串，和SQL语句配合实现两种情况适配
+            @RequestParam(value="keyword", defaultValue="") String keyword,
+            // pageNum默认值使用1
+            @RequestParam(value="pageNum", defaultValue="1") Integer pageNum,
+            // pageSize默认值使用5
+            @RequestParam(value="pageSize", defaultValue="5") Integer pageSize,
+            ModelMap modelMap) {
+        // 调用Service方法获取PageInfo对象
+        PageInfo<Admin> pageInfo = adminService.getPageInfo(keyword, pageNum, pageSize);
+        // 将PageInfo对象存入模型
+        modelMap.addAttribute(CrowdConstant.ATTR_NAME_PAGE_INFO, pageInfo);
+        return "admin-page";
+    }
+
+    /**
+     * 退出登录
+     * @param session
+     * @return
+     */
+    @RequestMapping("/admin/do/logout.html")
+    public String doLogout(HttpSession session) {
+        // 强制Session失效
+        session.invalidate();
+        return "redirect:/admin/to/login/page.html";
+    }
+
+    /**
+     * 管理员登录：http://localhost:8080/admin/to/login/page.html
+     * @param loginAcct 账号
+     * @param userPswd 密码
+     * @param session 会话
+     * @return 跳转视图
+     */
+    @RequestMapping("/admin/do/login.html")
+    public String doLogin(@RequestParam("loginAcct") String loginAcct,@RequestParam("userPswd") String userPswd, HttpSession session) {
+        // 调用Service方法执行登录检查
+        // 这个方法如果能够返回admin对象说明登录成功，如果账号、密码不正确则会抛出异常
+        Admin admin = adminService.getAdminByLoginAcct(loginAcct, userPswd);
+        // 将登录成功返回的admin对象存入Session域
+        session.setAttribute(CrowdConstant.ATTR_NAME_LOGIN_ADMIN, admin);
+        return "redirect:/admin/to/main/page.html";
+    }
+}
